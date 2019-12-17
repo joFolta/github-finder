13a74da6cf3bb63faf044351e778e70568ed6342
Johann Folta <jfolta@kyruus.com>
2019-12-16 20:34:24 -0500

    lists and passing state with props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  state = {
-    id: "40876495",
-    login: "joFolta",
-    avatar_url: "https://avatars2.githubusercontent.com/u/40876495?v=4",
-    html_url: "https://github.com/joFolta"
-  };
-
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
     return (
       <div>
         <div className="card text-center">
