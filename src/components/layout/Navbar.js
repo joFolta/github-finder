79c9519ed310407010a3b5f4a321d570c7f2a1bf
Johann Folta <jfolta@kyruus.com>
2019-12-16 21:09:01 -0500

    stateless functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github"
-  };
+//                const Navbar = props => {
+//                  const { icon, title } = props;
+// CLEANER DESTRUCTING BELOW...
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  render() {
-    console.log(this.props);
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
 
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div>
-        <div className="card text-center">
-          <img
-            src={avatar_url}
-            alt=""
-            className="round-img"
-            style={{ width: "60px" }}
-          />
-          <h3>{login}</h3>
+//        const UserItem = (props) => {
+//         const { login, avatar_url, html_url } = props.user;
+// CLEANER DESTRUCTING BELOW...
 
-          <div>
-            <a
-              href={html_url}
-              className="btn btn-dark btn-sm my-1"
-              target="_blank"
-            >
-              More
-            </a>
-          </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div>
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: "60px" }}
+        />
+        <h3>{login}</h3>
+
+        <div>
+          <a
+            href={html_url}
+            className="btn btn-dark btn-sm my-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
