cbfb5a7076e0bd9addd4e0c4fd97217a6c61a1c1
Johann Folta <jfolta@kyruus.com>
2020-01-18 15:31:46 -0500

    passing props up and search

--- src/App.js
+++ src/App.js
@@ -11,29 +11,24 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  // Search Github users
+  searchUsers = async text => {
     this.setState({ loading: true });
-
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    console.log(res);
-
-    this.setState({ users: res.data, loading: false });
-  }
-  render() {
     // TODO REMOVE LOG
-    console.log(
-      "REACT_APP_GITHUB_CLIENT_ID",
-      process.env.REACT_APP_GITHUB_CLIENT_ID
-    );
+    console.log("res", res);
+    this.setState({ users: res.data.items, loading: false });
+  };
 
+  render() {
     const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={loading} users={users} />
         </div>
       </div>
